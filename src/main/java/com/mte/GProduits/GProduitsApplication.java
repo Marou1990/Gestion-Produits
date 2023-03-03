a7aab34ccc112421fa8597d273d8f35fea780d7a
Marou1990 <marwen.teyeb90@gmail.com>
2023-03-03 14:18:10 +0100

    First mte 03-03

---
+++ src/main/java/com/mte/GProduits/GProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.mte.GProduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GProduitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mte/GProduits/GProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mte.GProduits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
